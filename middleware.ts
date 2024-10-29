5db53b3c351d9378f09a42943e7bdd01e9db4fbc
Sandeepon260 <sandeepon.saha@gmail.com>
2024-10-29 13:53:32 -0400

    webhook

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,8 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isProtectedRoute = createRouteMatcher(['/'])
 
+
+
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect()
 })
